f6f8d3c9c6db5d0b20c577bb25ece896796d0366
Francisco <franciscomartdomi@gmail.com>
2026-06-20 17:50:50 -0600

    Nueva variable y división

--- Ejemplo_tarea34.py
+++ Ejemplo_tarea34.py
@@ -1,7 +1,11 @@
 cadena = []
 a = 120 + cadena
+b = 140 / a
+
 frase = input('Escribe una frase: ')
 for palabra in frase:
     cadena.append(palabra)
 # se agrega variable a
+# se agregó variable b
+
 print(cadena)
